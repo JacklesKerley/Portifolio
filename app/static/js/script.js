1fe2f114138045981bc413f21a8b7a823b96d469
Jackles Kerley <jackleskerley@yahoo.com.br>
2022-11-18 16:10:31 -0300

    responsividade finalizada

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -6,11 +6,5 @@ let menuMobile = document.querySelector('.menuMobile')
 let aberto = false
 
 function abrirOuFecharMenu() {
-  if (aberto === false) {
-    aberto = true
-    menuMobile.classList.add('aberto')
-  } else {
-    aberto = false
-    menuMobile.classList.remove('aberto')
-  }
+  menuMobile.classList.toggle('aberto')
 }

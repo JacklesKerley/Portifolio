7f1a6eff0e170003e0522b145349e8508de4f3a3
Jackles Kerley <jackleskerley@yahoo.com.br>
2022-12-11 18:45:24 -0300

    deploy do projeto

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import app
+from app.app import app
 from flask import render_template
 
 @app.route('/')
@@ -8,16 +8,16 @@ def index():
 
 @app.route('/sobre')
 def lifestyle():
-  return render_template('lifestyle.html') 
+  return render_template('lifestyle.html')
 
 @app.route('/formacao')
 def academic():
-  return render_template('academic.html') 
+  return render_template('academic.html')
 
 @app.route('/experiencia')
 def works():
-  return render_template('works.html') 
+  return render_template('works.html')
 
 @app.route('/contato')
 def contato():
-  return render_template('contato.html') 
+  return render_template('contato.html')

583cbf07b05bc8c8fcbd759d925fead5548905ff
Jackles Kerley <jackleskerley@yahoo.com.br>
2024-03-13 12:38:41 -0300

    atualizei experiencias de trabalho

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -8,3 +8,12 @@ let aberto = false
 function abrirOuFecharMenu() {
   menuMobile.classList.toggle('aberto')
 }
+
+const elementosBoasVindas = document.querySelectorAll('.divBoasVindas');
+
+elementosBoasVindas.forEach((elemento, index) => {
+  setTimeout(() => {
+    console.log('funcionando')
+    elemento.classList.add('inicio')
+  }, index * 500)
+})
